3fb881ae969bbf90fbb9e18eae8196248a475a6a
Andrew Cope <andrew@thecope.net>
2018-01-21 00:59:34 -0800

    Add Component TableView

--- SourcedCode/ProductViewController.swift
+++ SourcedCode/ProductViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProductViewController: UIViewController {
+class ProductViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var product = Product()
     
@@ -61,15 +61,18 @@ class ProductViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! SearchCell
+        cell.titleLabel.text = product.components[indexPath.row].name
+        return cell
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return product.components.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
 }
